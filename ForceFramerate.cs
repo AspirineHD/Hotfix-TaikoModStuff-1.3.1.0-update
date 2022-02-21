adb511283fc7d9147a28bd4c9426962847aac167
Tawan Wareeboot <47499678+AspirineHD@users.noreply.github.com>
2022-02-22 05:14:28 +0700

    first-release

---
+++ ForceFramerate.cs
@@ -0,0 +1,44 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace TaikoUncap
+{
+    internal class ForceFramerate
+    {
+        private static readonly int VSyncFrames = Plugin.configToggleVSync.Value ? 1 : 0;
+
+        // Skip the original method, we're doing magic here
+        [HarmonyPatch(typeof(ForceRenderRate), "Start")]
+        [HarmonyPrefix]
+        static bool PrefixFocus()
+        {
+            return false;
+        }
+
+        // Skip the original method, we're doing magic here
+        [HarmonyPatch(typeof(ForceRenderRate), "Update")]
+        [HarmonyPrefix]
+        static bool PrefixScreenType()
+        {
+            return false;
+        }
+
+        [HarmonyPatch(typeof(ForceRenderRate), "Start")]
+        [HarmonyPrefix]
+        static void customStart()
+        {
+            // Force vsync at all times
+            QualitySettings.vSyncCount = VSyncFrames;
+            Application.targetFrameRate = Plugin.configCustomFramerate.Value;
+        }
+
+        [HarmonyPatch(typeof(ForceRenderRate), "Update")]
+        [HarmonyPrefix]
+        static void customUpdate()
+        {
+            // Force vsync at all times
+            QualitySettings.vSyncCount = VSyncFrames;
+            Application.targetFrameRate = Plugin.configCustomFramerate.Value;
+        }
+    }
+}
